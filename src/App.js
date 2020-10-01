fa8bc06d73515009f73d7a0bc48fc6b3d44c11bf
Wojciech Jarosz <jaroszw@gmail.com>
2020-10-01 22:54:09 +0200

    initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+import { connect } from 'react-redux';
+import { increment } from './actions';
+
 import './App.css';
 
-function App() {
+function App({ increment, myCount }) {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => increment()}>{myCount}</button>
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    myCount: state.count,
+  };
+};
+
+const mapDispatchToState = () => {
+  return {
+    increment,
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToState())(App);
